75e7b04be4017632ddc02ba76064ee94c5a9fe68
marialouisee <72508932+marialouisee@users.noreply.github.com>
2021-10-06 16:29:23 +0200

    image uploading still failing

--- blog-api/server.js
+++ blog-api/server.js
@@ -16,25 +16,25 @@ try {
   console.log(error)
 }
 
-app.use((err, req, res, next) => {
-  console.log(err.toString())
-  if (err.type === 'entity.too.large') {
-    res.send('this is the errorooooorrrr')
-  }
-  // if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-  //     if (err.type === 'entity.parse.failed') {
-  //         let data = req.body || req.query;
-  //         try {
-  //             JSON.parse(data); // <-- reproduce error in order to catch it
-  //         } catch (error) {
-  //             // get the first line of error which is "SyntaxError: Unexpected string in JSON at position 59"
-  //             let message = error.toString().split("\n")[0];
-  //             return res.status(400).send({ status: 400, message: message }); // Bad request
-  //         }
-  //     }            
-  //     else return res.status(400).send(err); // Bad request
-  // }
-});
+// app.use((err, req, res, next) => {
+//   console.log(err.toString())
+//   if (err.type === 'entity.too.large') {
+//     res.send('this is the errorooooorrrr')
+//   }
+//   // if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//   //     if (err.type === 'entity.parse.failed') {
+//   //         let data = req.body || req.query;
+//   //         try {
+//   //             JSON.parse(data); // <-- reproduce error in order to catch it
+//   //         } catch (error) {
+//   //             // get the first line of error which is "SyntaxError: Unexpected string in JSON at position 59"
+//   //             let message = error.toString().split("\n")[0];
+//   //             return res.status(400).send({ status: 400, message: message }); // Bad request
+//   //         }
+//   //     }            
+//   //     else return res.status(400).send(err); // Bad request
+//   // }
+// });
 
 
 app.use(
