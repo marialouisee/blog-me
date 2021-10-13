6a447619165e1b99d716fff1e604e13548da4862
marialouisee <72508932+marialouisee@users.noreply.github.com>
2021-10-13 14:57:28 +0200

    canvas and starting model import fiber

--- blog_client/src/pages/Home.js
+++ blog_client/src/pages/Home.js
@@ -1,25 +1,52 @@
-import React, {useContext, useEffect, useState } from 'react'
-import { UserContext } from "../context/UserProvider";
-import banana from '../images/pexels-banana.png'
-import bananaDive from '../images/pexels-banana-dive.png'
+import React, {useContext, useEffect, useState, useRef , Suspense} from 'react';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Html, useProgress } from '@react-three/drei'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight
+        castShadow
+        position={[0, 10, 0]}
+        intensity={1.5}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
+
+      <spotLight intensity={1} position={[100, 0, 0]} castShadow />
+    </>
+  );
+};
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, );
+  return (<Suspense fallback={null}>
+  <primitive object={gltf.scene} />;
+  </Suspense>)
+}
+
 
 const Home = () => {
-  const { user } = useContext(UserContext);
-  const [ backgroundImage, setBackgroundImage ] = useState(banana);
-
-  useEffect(() => {
-    if (user) {
-      setBackgroundImage(bananaDive);
-    } else {
-      setBackgroundImage(banana);
-    }
-  },[user])
+ 
 
   return (
-    <div className='home-bg' style={ {backgroundImage : `url(${backgroundImage})`} }>
+   <>
+   <Canvas>
+    <Lights/>
    
-      <h6>Blog-Me-If-You-Can</h6>
-    </div>
+   
+   </Canvas>
+   </>
   )
 }
 
