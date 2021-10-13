9f9ae3ac9b7fe92fd7ced3f3ec5886c820ef79c3
marialouisee <72508932+marialouisee@users.noreply.github.com>
2021-10-13 16:55:09 +0200

    three fiber model, html, text home component

--- blog_client/src/pages/Home.js
+++ blog_client/src/pages/Home.js
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect, useState, useRef , Suspense} from 'react';
-import { Canvas, useLoader } from "@react-three/fiber";
-import { Html, useProgress } from '@react-three/drei'
+import React, { useState, useRef , Suspense} from 'react';
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { Html, OrbitControls, Text } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 
@@ -10,44 +10,69 @@ const Lights = () => {
       <ambientLight intensity={0.3} />
 
       <directionalLight position={[10, 10, 5]} intensity={1} />
-      <directionalLight
-        castShadow
-        position={[0, 10, 0]}
-        intensity={1.5}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
 
-      <spotLight intensity={1} position={[100, 0, 0]} castShadow />
+      <spotLight intensity={1.3} position={[10, 0, 0]} color="red"  />
     </>
   );
 };
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, );
-  return (<Suspense fallback={null}>
-  <primitive object={gltf.scene} />;
-  </Suspense>)
-}
+const  Model = () =>  {
+  const gltf = useLoader(GLTFLoader, '/scene.gltf');
+  // console.log(gltf)
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.y  += 0.01));
+
+  return (
+    <mesh ref={mesh}>
+      <primitive position={[0, 0, 1]} object={gltf.scene} scale={15} />
+    </mesh>
+  );
+};
+
+const HTMLContent = () => {
+  return (
+    <group position={[-1, 0.5, 2]}>
+      <Html  >
+        <h1>When life gives you lemons....</h1>
+        <p>...leave us a blog post. Just be nice.</p>
+      </Html>
+    </group>
+  );
+};
+
+const Hello = () => {
+  const mesh = useRef(null);
+  const [active, setActive] = useState(false)
+
+  useFrame(() => { mesh.current.rotation.y -= 0.009;
+    // textRef.current.rotation.x += 0.005;
+  });
+  return (
+    <Text onClick={(e) => setActive(!active)} scale={active ? 15 : 10} color={active? 'black' : '#1b5f56'} anchorX="center" anchorY="middle" ref={mesh}>
+      BLOG ME IF YOU CAN
+    </Text>
+  );
+};
 
 
 const Home = () => {
- 
 
   return (
-   <>
-   <Canvas>
-    <Lights/>
-   
-   
-   </Canvas>
-   </>
-  )
+    <div className='canvas'>
+      <Canvas shadows
+          colorManagement
+          camera={{ position: [2, 1, 0], fov: 60 }}
+          >
+        <Suspense fallback={null}>
+          <Lights />
+          <Model />
+          <OrbitControls />
+          <HTMLContent/>
+          <Hello/>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default Home
